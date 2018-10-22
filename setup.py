86a5001ac8ef122046605b4b8b2d00bf73932323
kojirof <kojirof@gumob.com>
2018-10-22 23:59:03 +0900

    Bumped version to 0.1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def requirements(filename):
 
 setup(
     name='music-dl',
-    version='0.1.3',
+    version='0.1.4',
     description='Command line tool to download music from YouTube and SoundCloud',
     long_description=read_file('README.md'),
     url='https://github.com/gumob/music-dl',
