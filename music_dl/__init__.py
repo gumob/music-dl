16489ed83970777fbade3050f129b9c1ae3f0351
kojirof <kojirof@gumob.com>
2018-10-23 01:57:59 +0900

    Bumped version to 0.1.9

--- music_dl/__init__.py
+++ music_dl/__init__.py
@@ -9,7 +9,7 @@ import pkg_resources
 
 from music_dl.MusicDL import MusicDL
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 __license__ = 'MIT'
 __author__ = 'Gumob'
 __author_email__ = 'hello@gumob.com'
