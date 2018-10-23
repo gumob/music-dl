dc60cbb97dabc0a57ca42ced0f08ae4108e28622
kojirof <kojirof@gumob.com>
2018-10-23 13:11:44 +0900

    Only pure argparse for debugging

--- music_dl/__init__.py
+++ music_dl/__init__.py
@@ -9,7 +9,7 @@ import pkg_resources
 
 from music_dl.MusicDL import MusicDL
 
-__version__ = '0.1.25'
+__version__ = '0.1.26'
 __license__ = 'MIT'
 __author__ = 'Gumob'
 __author_email__ = 'hello@gumob.com'
@@ -81,21 +81,21 @@ def main():
     # args.dir = args.dir if args.dir is not None else default_dir
 
     # Execute download
-    mdl = MusicDL(
-        download_url=args.url,
-        working_dir=args.dir,
-        audio_codec=args.codec,
-        audio_bitrate=args.bitrate,
-        playlist_start=int(args.playlist_start),
-        playlist_end=int(args.playlist_end),
-        no_artwork=args.no_artwork,
-        no_album_title=args.no_album_title,
-        no_album_artist=args.no_album_artist,
-        no_track_number=args.no_track_number,
-        no_composer=args.no_composer,
-        no_compilation=args.no_compilation,
-        open_dir=args.open_dir,
-        # clear_cache=args.clear_cache,
-        verbose=args.verbose
-    )
-    mdl.download()
+    # mdl = MusicDL(
+    #     download_url=args.url,
+    #     working_dir=args.dir,
+    #     audio_codec=args.codec,
+    #     audio_bitrate=args.bitrate,
+    #     playlist_start=int(args.playlist_start),
+    #     playlist_end=int(args.playlist_end),
+    #     no_artwork=args.no_artwork,
+    #     no_album_title=args.no_album_title,
+    #     no_album_artist=args.no_album_artist,
+    #     no_track_number=args.no_track_number,
+    #     no_composer=args.no_composer,
+    #     no_compilation=args.no_compilation,
+    #     open_dir=args.open_dir,
+    #     # clear_cache=args.clear_cache,
+    #     verbose=args.verbose
+    # )
+    # mdl.download()
