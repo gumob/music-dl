4530d6ee53a66bf0c1635273f0a71f85c56f229e
kojirof <kojirof@gumob.com>
2018-10-23 13:14:21 +0900

    Enable only help

--- music_dl/__init__.py
+++ music_dl/__init__.py
@@ -9,7 +9,7 @@ import pkg_resources
 
 from music_dl.MusicDL import MusicDL
 
-__version__ = '0.1.27'
+__version__ = '0.1.28'
 __license__ = 'MIT'
 __author__ = 'Gumob'
 __author_email__ = 'hello@gumob.com'
@@ -33,15 +33,15 @@ def main():
     parser = argparse.ArgumentParser(
         prog='music-dl',
         description='Music Downloader - Command line tool to download music from YouTube and SoundCloud',
-        # add_help=True,
-        add_help=False,
+        add_help=True,
+        # add_help=False,
         epilog=pkg_info,
-        formatter_class=CapitalisedHelpFormatter,
+        # formatter_class=CapitalisedHelpFormatter,
     )
 
-    parser._positionals.title = 'Positional arguments'
-    parser._optionals.title = 'Optional arguments'
-    argparse._HelpAction(option_strings=['-h', '--help'], dest='help', default='==SUPPRESS==', help='Show this help message and exit.')
+    # parser._positionals.title = 'Positional arguments'
+    # parser._optionals.title = 'Optional arguments'
+    # argparse._HelpAction(option_strings=['-h', '--help'], dest='help', default='==SUPPRESS==', help='Show this help message and exit.')
 
     # parser.add_argument('-u', '--url', action='store', type=str,
     #                     help='URL to download. Without this argument, URL is read from clipboard.')
@@ -73,8 +73,8 @@ def main():
     #                     help='Clear cache directory.')
     # parser.add_argument('--verbose', action='store_true',
     #                     help='Print verbose message.')
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                        help='Show this help message and exit.')
+    # parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+    #                     help='Show this help message and exit.')
 
     args = parser.parse_args()
     # args.url = args.url if args.url is not None else clipboard.paste()
